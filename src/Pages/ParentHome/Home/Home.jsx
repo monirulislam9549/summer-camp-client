cdae59a23fddff9675d22b358ba3b0d11f46283a
monirulislam9549 <monirulislam9549@gmail.com>
2023-06-12 23:11:04 +0600

    framer motion added

--- src/Pages/ParentHome/Home/Home.jsx
+++ src/Pages/ParentHome/Home/Home.jsx
@@ -2,14 +2,21 @@ import { Helmet } from "react-helmet-async";
 import Banner from "../Banner/Banner";
 import PopularInstructors from "../PopularInstructors/PopularInstructors";
 import ExtraSection from "../ExtraSection/ExtraSection";
+import { useScroll, motion } from "framer-motion";
 
 const Home = () => {
+  const { scrollYProgress } = useScroll;
   return (
     <div>
       <>
         <Helmet>
           <title>Summer Camp | Home</title>
         </Helmet>
+
+        <motion.div
+          className="progress-bar"
+          style={{ scaleX: scrollYProgress }}
+        />
         <Banner></Banner>
         <PopularInstructors></PopularInstructors>
         <ExtraSection></ExtraSection>
